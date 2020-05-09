55d7d752bb52a2596f617f0bf7b93b33abc8c224
unknown <dewiagustini.id@gmail.com>
2020-05-09 09:18:53 +0800

    four commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,5 @@
-import 'src/Kasir.dart';
-import 'src/Menu.dart';
-import 'src/lib/about_me.dart';
-import 'src/Bayar.dart';
+import 'package:duarrr/src/app.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Kasir Sederhana',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(body: Menu()),
-        routes: <String, WidgetBuilder>{
-          '/menu': (BuildContext context) => new Menu(),
-          '/kasir': (BuildContext context) => new Kasir(),
-          '/tentang': (BuildContext context) => new MyProfil(),
-          '/bayar': (BuildContext context) => new Bayar(),
-        }
-    );
-
-  }
-}
+void main() => runApp(App());
